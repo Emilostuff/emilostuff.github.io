dd20a40c30aa34de7b135c69969b4d86d818476e
Emilostuff <emilostuff@gmail.com>
2022-06-29 23:51:40 +0200

    fixed hero picture for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 
 
-$("a[href='#bottom']").click(function() {
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    return false;
-  });
+
 
 
 (function ($) {
     'use strict';
 
-    
+    $("a[href='#bottom']").click(function() {
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+        return false;
+      });
 
 
 
