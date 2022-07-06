63679f54aaccd05404c7ae6f94b9456ea8107e4c
Emilostuff <Emilostuff@users.noreply.github.com>
2022-07-06 10:53:42 +0000

    deploy: d32e2dd0a8ddff071e00ff59fc37223d670f4fbb

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+
+(function ($) {
+    'use strict';
+
+    // Scroll to bottom
+    $("a[href='#bottom']").click(function () {
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+        // reset focus
+        document.activeElement.blur();
+        return false;
+    });
+
+
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function () {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    // progress bar
+    $('[data-progress]').each(function () {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
+
+    // Shuffle js filter and masonry
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+        itemSelector: '.shuffle-item',
+        buffer: 1
+    });
+
+    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+        var input = evt.currentTarget;
+        if (input.checked) {
+            myShuffle.filter(input.value);
+        }
+    });
+
+})(jQuery);
